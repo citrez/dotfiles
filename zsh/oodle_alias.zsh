48e942b71839371611281a43e5d42a5eb3cbcbf1
Ezra Citron <ezracitron@gmail.com>
2022-03-17 10:35:53 +0000

    moving to oodle_aliases

--- zsh/oodle_alias.zsh
+++ zsh/oodle_alias.zsh
@@ -3,6 +3,8 @@ export ds="~/Documents/repos/oodle-data-science-projects"
 # oodle
 alias awslogin="aws-azure-login -a --no-prompt"
 alias clone_data_flow="git clone git@github.com:oodlefinance/oodle-data-flow.git"
+export affordability="$HOME/Documents/repos/affordability"
+export policy_rules = "$HOME/Documents/repos/policy_rules"
 
 #echo "AWS_PROFILE=dev"
 export AWS_PROFILE=dev
