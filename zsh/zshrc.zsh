48782d959de5210362398df48b900906548796f3
Ezra Citron <ezracitron@gmail.com>
2022-05-23 15:21:04 +0100

    comment out conda

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -2,7 +2,7 @@ clear #remove the last login message
 ## LOAD OTHER FILES, FOR OGANISING
 source  ~/dotfiles/zsh/alias.zsh
 # OODLE
-source ~/dotfiles/zsh/oodle_alias.zsh
+# source ~/dotfiles/zsh/oodle_alias.zsh
 
 # Find and set branch name var if in git repository.
 function git_branch_name()
@@ -41,16 +41,18 @@ export PROMPT='${COLOR_DIR}%B%~%b ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF} %%
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/ezracitron/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/ezracitron/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/ezracitron/opt/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/ezracitron/opt/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
+#__conda_setup="$('/Users/ezracitron/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+#if [ $? -eq 0 ]; then
+#    eval "$__conda_setup"
+#else
+#    if [ -f "/Users/ezracitron/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+#        . "/Users/ezracitron/opt/anaconda3/etc/profile.d/conda.sh"
+#    else
+#        export PATH="/Users/ezracitron/opt/anaconda3/bin:$PATH"
+#    fi
+#fi
+#unset __conda_setup
 # <<< conda initialize <<<
 
+
+export PATH="$HOME/.poetry/bin:$PATH"
