44e77d292bc7d9642e6941dea9b0d395142c246f
Ezra Citron <ezracitron@gmail.com>
2023-12-05 13:15:15 +0000

    add learning alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -5,7 +5,7 @@ alias sve="source .venv/bin/activate"
 alias rve="rm -rf .venv"
 
 alias c="clear"
-alias todo="python -m todo_cli"
+# alias todo="python -m todo_cli"
 # alias pip=/usr/local/bin/pip3
 alias pip=pip3
 
@@ -16,7 +16,7 @@ alias ez="vim ~/dotfiles/zsh/zshrc.zsh  && source ~/dotfiles/zsh/zshrc.zsh"
 alias eb="vim ~/dotfiles/brew/brew.sh  && source ~/dotfiles/zsh/brew.sh"
 alias eg="vim ~/dotfiles/git/gitconfig  && source ~/dotfiles/git/gitconfig"
 alias ev="vim ~/dotfiles/vim/vimrc.vim"
-alias eik="code ~/my_projects/Everything-I-Know"
+# alias eik="code ~/my_projects/Everything-I-Know"
 
 #DIRECTORY
 alias d="cd"
@@ -45,17 +45,16 @@ alias gc="git commit"
 alias glg="git log --graph --decorate --oneline --abbrev-commit"
 
 # Lean
-alias lcpush="lean cloud push"
-alias lcpull="lean cloud pull"
-alias lrb="lean research backtest"
-
+alias lcs="lean cloud push --project"
+alias lcp="lean cloud pull --project"
+alias lcb="lean cloud backtest --push" 
 
 
 # My variables
 export repos="$HOME/Documents/repos/"
 export my_projects="$HOME/my_projects/"
+export learning="$HOME/my_projects/learning"
 export snippet2="$HOME/Documents/my-projects/snippets2/src/snippets2"
-
 
 # Other 
 alias v="vim"
@@ -72,4 +71,3 @@ gcm() {
 }
 
 # source other zsh files
-
