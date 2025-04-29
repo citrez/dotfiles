ba61cea1e29315de13722c04941412a0ab1450eb
Ezra Citron <48212179+citrez@users.noreply.github.com>
2025-04-29 12:27:10 +0100

    com

--- brew/brew.sh
+++ brew/brew.sh
@@ -1,18 +1,19 @@
 # Install Brew Packages
-brew install python
+# brew install python
 brew install tree
 
 # Install MacOS Applications
-brew install --cask google-chrome
-brew install --cask spotify
-brew install --cask gimp
+# brew install --cask google-chrome
+# brew install --cask spotify 
+# brew install --cask gimp
 brew install --cask visual-studio-code
 brew install --cask scroll-reverser
 brew install --cask rectangle
-brew install youtube-dl
-brew install ffmpeg
-brew install --cask fliqlo
-brew install bat
-brew install fd
-brew install pyenv
-brew install git-gui
+brew install --cask obsidian
+# brew install youtube-dl
+# brew install ffmpeg
+# brew install --cask fliqlo
+# brew install bat
+# brew install fd
+# brew install pyenv
+# brew install git-gui
